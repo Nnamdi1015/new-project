7e1d91163e83c0a7f8bde92ec906d2f32f934d2e
Nnamdi <nnamdiamadi391@gmail.com>
2025-12-04 04:01:24 -0800

    new-project

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ h1Elem.style.color = "blue";
 h2Elem.innerHTML = "The Kidnapping Pandemic!";
 
 // grab input elements
+
 let InputFN = document.getElementById("firstname");
 let InputLN = document.getElementById("lastname");
 let spanElem = document.getElementById("showFN");
@@ -24,7 +25,23 @@ spanElem.style.fontWeight = "900";
 //let newFNValue = InputFN.value;
 //let newLNValue = InputLN.value;
 
-function displayFullname(){
+function displayFullname() {
     let fullname = InputFN.value + " " + InputLN.value;
     spanElem.innerHTML = fullname;
+}
+//number inputs
+let num1Elem = document.getElementById("num1");
+let num2Elem = document.getElementById("num2");
+let spanElem = document.getElementById("showresult");
+
+console.log(num1Elem);
+console.log(num2Elem);
+
+spanElem.style.color = "red";
+spanElem.style.fontWeight = "900";
+
+function displaynumber() {
+let num1 = inputnum1.value + " " + inputnum2.value;
+spanElem.innerHTML = displaynumber;
+
 }
