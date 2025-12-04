edcd20d0ca9f6ae099948b10d7f780cb722ced23
Nnamdi <nnamdiamadi391@gmail.com>
2025-12-04 03:03:11 -0800

    new-project

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+let h1Elem = document.getElementById("h1text");
+let h2Elem = document.getElementById("h2text");
+
+h1Elem.innerHTML = "My Blog Title";
+h1Elem.style.color = "blue";
+
+h1Elem.innerHTML = "The Kidnapping Pandemic !";
+
+// grab input elements
+let InputFN = document.getElementById("firstname");
+let InputLN = document.getElementById("lastname");
+let spanElem = document.getElementById("showFN");
+
+console.log(InputFN);
+console.log(InputLN);
+
+spanElem.style.color = "green";
+spanElem.style.fontWeight = "900";
+
+
+
+//let newFNValue = InputFN.value;
+//let newLNValue = InputLN.value;
+
+functiondisplayFullName()
+ {
+    let fullname = InputFN.value + " " + InputLN.value;
+    spanElem.innerHTML = fullname;
+}
