a22ff0c25dfdb1d57ad5694849b9ef9ac5942a4c
Nnamdi <nnamdiamadi391@gmail.com>
2025-12-05 00:43:17 -0800

    new-project

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-
+function displayFN() {
+    document.getElementById("fname").innerHTML = "amadi";
+    document.getElementById("lname").innerHTML = "samuel";
+}
 
 let h1Elem = document.getElementById("h1text");
 let h2Elem = document.getElementById("h2text");
@@ -10,8 +13,8 @@ h2Elem.innerHTML = "The Kidnapping Pandemic!";
 
 // grab input elements
 
-let InputFN = document.getElementById("firstname");
-let InputLN = document.getElementById("lastname");
+let InputFN = document.getElementById("Firstname");
+let InputLN = document.getElementById("Lastname");
 let spanElem = document.getElementById("showFN");
 
 console.log(InputFN);
@@ -21,27 +24,23 @@ spanElem.style.color = "green";
 spanElem.style.fontWeight = "900";
 
 
-
 //let newFNValue = InputFN.value;
 //let newLNValue = InputLN.value;
 
-function displayFullname() {
-    let fullname = InputFN.value + " " + InputLN.value;
-    spanElem.innerHTML = fullname;
+function displayFullName() {
+    let fullName = InputFN.value + " " + InputLN.value;
+    spanElem.innerHTML = fullName;
 }
-//number inputs
-let num1Elem = document.getElementById("num1");
-let num2Elem = document.getElementById("num2");
-let spanElem = document.getElementById("showresult");
+// grab input elements
 
-console.log(num1Elem);
-console.log(num2Elem);
+let Number1 = document.getElementById("num1");
+let Number2 = document.getElementById("num2");
+let sumspan = document.getElementById("displaysum");
 
-spanElem.style.color = "red";
-spanElem.style.fontWeight = "900";
+sumspan.style.color = "red";
+sumspan.style.fontWeight = "900";
 
-function displaynumber() {
-let num1 = inputnum1.value + " " + inputnum2.value;
-spanElem.innerHTML = displaynumber;
-
+function displaysum() {
+    let displaysum = Number(num1.value) + Number(num2.value);
+    sumspan.innerHTML = displaysum;
 }
